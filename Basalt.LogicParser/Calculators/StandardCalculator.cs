4a8b7d97d316606b8681606a2bb0dbaa15601926
unknown <brandenek03912@hotmail.com>
2024-06-15 09:31:18 -0400

    calculate: create and use interface

---
+++ Basalt.LogicParser/Calculators/StandardCalculator.cs
@@ -0,0 +1,51 @@
+﻿using Basalt.LogicParser.Models;
+using System.Collections.Generic;
+
+namespace Basalt.LogicParser.Calculators;
+
+/// <inheritdoc/>
+public class StandardCalculator : ICalculator
+{
+    /// <inheritdoc/>
+    public bool Calculate(string expression, IEnumerable<Token> tokens)
+    {
+        var stack = new Stack<Variable>();
+
+        // Process each token in the list and add its result to the stack
+        foreach (var token in tokens)
+        {
+            ProcessToken(expression, token, stack);
+        }
+
+        // Ensure that the stack is fully completed
+        if (stack.Count != 1)
+            throw new LogicParserException("Order of operations was incorrect for expression: " + expression);
+
+        return (stack.Pop() as BoolVariable).Value;
+    }
+
+    /// <summary>
+    /// Handles the logic of the surrounding tokens and pushes the result to the stack
+    /// </summary>
+    private void ProcessToken(string expression, Token token, Stack<Variable> stack)
+    {
+        // Variable token
+        if (token is Variable var)
+        {
+            stack.Push(var);
+            return;
+        }
+
+        // Operator token
+        if (token is Operator op)
+        {
+            if (stack.Count < 2)
+                throw new LogicParserException("Order of operations was incorrect for expression: " + expression);
+
+            Variable right = stack.Pop(), left = stack.Pop();
+            bool result = op.Evaluate(left, right);
+            stack.Push(new BoolVariable(result));
+            return;
+        }
+    }
+}
--- Basalt.LogicParser/InventoryData.cs
+++ Basalt.LogicParser/InventoryData.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Basalt.LogicParser.Models;
+﻿using Basalt.LogicParser.Calculators;
 using Basalt.LogicParser.Parsers;
 using Basalt.LogicParser.Resolvers;
 
@@ -16,65 +15,18 @@ public abstract class InventoryData
     public bool Evaluate(string expression)
     {
         // If nothing, then its true
-        if (expression == null || expression == string.Empty)
-        {
+        if (string.IsNullOrEmpty(expression))
             return true;
-        }
 
-        IParser parser = new StandardParser(new StandardResolver(this));
+        IResolver resolver = new StandardResolver(this);
+        IParser parser = new StandardParser(resolver);
+        ICalculator calculator = new StandardCalculator();
 
-        var tokens = parser.Parse(expression);
-
-        return ProcessTokens(expression, tokens);
+        return calculator.Calculate(expression, parser.Parse(expression));
     }
 
     /// <summary>
     /// Get an object representing the value of the specified variable from the child class
     /// </summary>
     protected internal abstract object GetVariable(string variable);
-
-    /// <summary>
-    /// Evaluate the tokens in postfix notation as a stack until the final result is left
-    /// </summary>
-    private bool ProcessTokens(string expression, IEnumerable<Token> tokens)
-    {
-        var stack = new Stack<Variable>();
-
-        // Process each token in the list and add its result to the stack
-        foreach (var token in tokens)
-        {
-            ProcessToken(expression, token, stack);
-        }
-
-        // Ensure that the stack is fully completed
-        if (stack.Count != 1)
-            throw new LogicParserException("Order of operations was incorrect for expression: " + expression);
-
-        return (stack.Pop() as BoolVariable).Value;
-    }
-
-    /// <summary>
-    /// Handles the logic of the surrounding tokens and pushes the result to the stack
-    /// </summary>
-    private void ProcessToken(string expression, Token token, Stack<Variable> stack)
-    {
-        // Variable token
-        if (token is Variable var)
-        {
-            stack.Push(var);
-            return;
-        }
-
-        // Operator token
-        if (token is Operator op)
-        {
-            if (stack.Count < 2)
-                throw new LogicParserException("Order of operations was incorrect for expression: " + expression);
-
-            Variable right = stack.Pop(), left = stack.Pop();
-            bool result = op.Evaluate(left, right);
-            stack.Push(new BoolVariable(result));
-            return;
-        }
-    }
 }
