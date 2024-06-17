77f58589b5f5c413cb6e9c9463f9d805ffed7284
unknown <brandenek03912@hotmail.com>
2024-06-17 09:51:26 -0400

    inventory: finalize

--- Basalt.LogicParser/GameInventory.cs
+++ Basalt.LogicParser/GameInventory.cs
@@ -16,32 +16,12 @@ public class GameInventory(ICalculator calculator, IFormatter formatter, IParser
     private readonly IResolver _resolver = resolver;
 
     /// <summary>
-    /// Used to calculate a boolean result from a collection of tokens
-    /// </summary>
-    public ICalculator Calculator { get; set; } = new StandardCalculator();
-
-    /// <summary>
-    /// Used to format the string expression before going into the parser
-    /// </summary>
-    public IFormatter Formatter { get; set; } = new ParenthesisPaddingFormatter();
-
-    /// <summary>
-    /// Used to parse the string expression into a collection of tokens
-    /// </summary>
-    public IParser Parser { get; set; } = new StandardParser();
-
-    /// <summary>
-    /// Used to resolve a string variable into a token
-    /// </summary>
-    public IResolver Resolver { get; set; } = new EmptyResolver();
-
-    /// <summary>
     /// Converts the string expression to a logical statement and evaluates it
     /// </summary>
     public bool Evaluate(string expression)
     {
         LogicParserException.CurrentExpression = expression;
 
-        return string.IsNullOrEmpty(expression) || Calculator.Calculate(Parser.Parse(Formatter.Format(expression), Resolver));
+        return string.IsNullOrEmpty(expression) || _calculator.Calculate(_parser.Parse(_formatter.Format(expression), _resolver));
     }
 }
