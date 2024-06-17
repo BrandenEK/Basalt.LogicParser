5e38a2db4c505c901dc678f9103a5dbdfc316318
unknown <brandenek03912@hotmail.com>
2024-06-17 11:27:07 -0400

    collect: add reflection implementation

---
+++ Basalt.LogicParser/Collectors/ReflectionCollector.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Basalt.LogicParser.Collectors;
+
+/// <summary>
+/// Adds or removes items based on collectable attributes on the info object
+/// </summary>
+public class ReflectionCollector(object info) : ICollector
+{
+    private readonly object _info = info;
+
+    /// <inheritdoc/>
+    public void Add(string item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public void Remove(string item)
+    {
+        throw new System.NotImplementedException();
+    }
+}
