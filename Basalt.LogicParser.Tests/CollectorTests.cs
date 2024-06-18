37c6562c82736097338f499902a725399756d39a
unknown <brandenek03912@hotmail.com>
2024-06-18 09:54:20 -0400

    test: empty collector tests

--- Basalt.LogicParser.Tests/CollectorTests.cs
+++ Basalt.LogicParser.Tests/CollectorTests.cs
@@ -9,50 +9,12 @@ public abstract class CollectorTests
     private ICollector Collector => _collector ?? throw new NullReferenceException(nameof(_collector));
 
     [TestMethod]
-    public void Execute()
+    [DataRow(new string[] { }, "fff00")]
+    public void Execute(string[] input, string output)
     {
         Collector.Add("");
     }
 }
-
-//public abstract class CalculatorTests
-//{
-
-//    [TestMethod]
-//    [DataRow("", OutputType.Error)]
-//    [DataRow("t", OutputType.True)]
-//    [DataRow("f", OutputType.False)]
-//    [DataRow("1", OutputType.Error)]
-//    [DataRow("(", OutputType.Error)]
-//    [DataRow("+", OutputType.Error)]
-//    [DataRow(">", OutputType.Error)]
-//    [DataRow("tt", OutputType.Error)]
-//    [DataRow("()", OutputType.Error)]
-//    [DataRow("f+", OutputType.Error)]
-//    [DataRow(")|", OutputType.Error)]
-//    [DataRow("tt+", OutputType.True)]
-//    [DataRow("tt|", OutputType.True)]
-//    [DataRow("tf+", OutputType.False)]
-//    [DataRow("ft|", OutputType.True)]
-//    [DataRow("10>", OutputType.True)]
-//    [DataRow("tt+f|", OutputType.True)]
-//    public void Calculate_Postfix(string input, OutputType output)
-//    {
-//        var tokens = ParseString(input);
-//        switch (output)
-//        {
-//            case OutputType.True:
-//                Assert.IsTrue(Calculator.Calculate(tokens));
-//                break;
-//            case OutputType.False:
-//                Assert.IsFalse(Calculator.Calculate(tokens));
-//                break;
-//            case OutputType.Error:
-//                Assert.ThrowsException<LogicParserException>(() => Calculator.Calculate(tokens));
-//                break;
-//        }
-//    }
-//}
 
 [TestClass]
 public class ReflectionCollectorTests : CollectorTests
@@ -60,6 +22,6 @@ public class ReflectionCollectorTests : CollectorTests
     [TestInitialize]
     public void Setup()
     {
-        _collector = new ReflectionCollector(new object());
+        _collector = new ReflectionCollector(new TestInventoryInfo());
     }
 }
--- Basalt.LogicParser.Tests/TestInventoryInfo.cs
+++ Basalt.LogicParser.Tests/TestInventoryInfo.cs
@@ -1,12 +1,22 @@
 ﻿
+using Basalt.LogicParser.Attributes;
+
 namespace Basalt.LogicParser.Tests;
 
 public class TestInventoryInfo
 {
+    [CollectableAs("a")]
     public bool A { get; set; }
+
+    [CollectableAs("b")]
     public bool B { get; set; }
+
+    [CollectableAs("c")]
     public bool C { get; set; }
 
+    [CollectableAs("x")]
     public int X { get; set; }
+
+    [CollectableAs("y")]
     public int Y { get; set; }
 }
